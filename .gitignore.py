9bfcf17e5feaa904bef23653425841eb743614ed
Ahtii <ahtishamshafi9906@gmail.com>
2024-01-21 21:30:40 +0530

    updated gitignore

--- .gitignore.py
+++ .gitignore.py
@@ -1,3 +1,37 @@
 geckodriver.log
 db.sqlite3
-.idea
+.idea/
+/__pycache__
+*.log
+*.pot
+*.pyc
+__pycache__
+db.sqlite3
+media
+# If you are using PyCharm #
+# User-specific stuff
+.idea/**/workspace.xml
+.idea/**/tasks.xml
+.idea/**/usage.statistics.xml
+.idea/**/dictionaries
+.idea/**/shelf
+
+# AWS User-specific
+.idea/**/aws.xml
+
+# Generated files
+.idea/**/contentModel.xml
+
+# Sensitive or high-churn files
+.idea/**/dataSources/
+.idea/**/dataSources.ids
+.idea/**/dataSources.local.xml
+.idea/**/sqlDataSources.xml
+.idea/**/dynamic.xml
+.idea/**/uiDesigner.xml
+.idea/**/dbnavigator.xml
+
+# Gradle
+.idea/**/gradle.xml
+.idea/**/libraries
+.idea/superlists.iml

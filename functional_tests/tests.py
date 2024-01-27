0bd21a59531e64499ac93bb4b88c632563c3c85a
Ahtii <ahtishamshafi9906@gmail.com>
2024-01-27 23:31:42 +0530

    first setps of FT for layout + styling

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -82,3 +82,23 @@ class NewVisitorTest(LiveServerTestCase):
         page_text = self.browser.find_element('tag name', 'body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertIn('Buy milk', page_text)
+
+    def test_layout_and_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        input_box = self.browser.find_element('id', 'id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
+        input_box.send_keys('testing')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_tables('1: testing')
+        input_box = self.browser.find_element('id', 'id_new_item')
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )

441a58431e7d2e6aa7e903d0cb39018a26265e57
Ahtii <ahtishamshafi9906@gmail.com>
2024-01-27 11:48:37 +0530

    Add list and list item view with tests

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -30,6 +30,25 @@ class NewVisitorTest(LiveServerTestCase):
                 time.sleep(0.5)
 
     def test_can_start_a_list_for_one_user(self):
+        self.browser.get(self.live_server_url)
+
+        self.assertIn('To-Do', self.browser.title)
+        header_text = self.browser.find_element('tag name', 'h1').text
+        self.assertIn('To-Do', header_text)
+
+        input_box = self.browser.find_element('id', 'id_new_item')
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+        input_box.send_keys('Buy peacock feathers')
+        input_box.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_tables('1: Buy peacock feathers')
+
+        input_box = self.browser.find_element('id', 'id_new_item')
+        input_box.send_keys('Use peacock feathers to make a fly')
+        input_box.send_keys(Keys.ENTER)
+
         self.wait_for_row_in_list_tables('2: Use peacock feathers to make a fly')
         self.wait_for_row_in_list_tables('1: Buy peacock feathers')
 
@@ -62,30 +81,4 @@ class NewVisitorTest(LiveServerTestCase):
 
         page_text = self.browser.find_element('tag name', 'body').text
         self.assertNotIn('Buy peacock feathers', page_text)
-        self.assertNotIn('Buy milk', page_text)
-
-    def test_can_start_a_list_and_retrieve_it_later(self):
-        self.browser.get(self.live_server_url)
-        self.assertIn('To-Do', self.browser.title)
-
-        header_text = self.browser.find_element('tag name', 'h1').text
-        self.assertIn('To-Do', header_text)
-
-        input_box = self.browser.find_element('id', 'id_new_item')
-        self.assertEqual(
-            input_box.get_attribute('placeholder'),
-            'Enter a to-do item'
-        )
-
-        input_box.send_keys('Buy peacock feathers')
-        input_box.send_keys(Keys.ENTER)
-        self.wait_for_row_in_list_tables('1: Buy peacock feathers')
-
-        input_box = self.browser.find_element('id', 'id_new_item')
-        input_box.send_keys('Use peacock feathers to make a fly')
-        input_box.send_keys(Keys.ENTER)
-
-        self.wait_for_row_in_list_tables('2: Use peacock feathers to make a fly')
-        self.wait_for_row_in_list_tables('1: Buy peacock feathers')
-
-        self.fail('Finish the test!')
+        self.assertIn('Buy milk', page_text)

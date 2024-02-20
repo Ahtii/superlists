4abc1fed7bade4ed07754ada347abf81ed2b848a
Ahtii <ahtishamshafi9906@gmail.com>
2024-02-20 21:31:21 +0530

    Added wait_for and list in list item validation

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -1,7 +1,29 @@
+from selenium.webdriver.common.keys import Keys
+
 from functional_tests.base import FunctionalTest
 
 
 class NewVisitorTest(FunctionalTest):
 
     def test_cannot_add_empty_list_items(self):
-        self.fail("write me")
+        self.browser.get(self.live_server_url)
+        self.browser.find_element('id', 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element('class name', 'has-error').text,
+            "You can't have an empty list item"
+        ))
+        self.browser.find_element('id', 'id_new_item').send_keys('Buy milk')
+        self.browser.find_element('id', 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_tables('1: Buy milk')
+
+        self.browser.find_element('id_new_item').send_keys(Keys.ENTER)
+
+        self.wait_for(lambda: self.assertEqual(
+            self.browser.find_element('class name', 'has-error').text,
+            "You can't have an empty list item"
+        ))
+
+        self.browser.find_element('id', 'id_new_item').send_keys('Make tea')
+        self.browser.find_element('id', 'id_new_item').send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_tables('1: Buy milk')
+        self.wait_for_row_in_list_tables('1: Make tea')
